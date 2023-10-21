e3bee6ce1b7a25fcc26894b8628033e7b77fbf21
josephcranston <jcransto@syr.edu>
2023-10-21 12:17:46 -0400

    Initial commit for Map class
    -Updated src/main.cpp to include code to run both Map and Reduce class
    -Updated makefile to run Map unit test
    -Updated makefile to tokenize the All'sWellThatEndsWell.txt file. The OutputFile.txt file should contain the proper outputs to be fed into the Sorter.

--- Makefile
+++ Makefile
@@ -8,13 +8,13 @@ build:
 	-DBOOST_LOG_DYN_LINK -lboost_log -lboost_log_setup -lboost_thread -lboost_system
 
 build-test:
-	@ g++ src/Reduce.cpp tests/*.cpp -o bin/test
+	@ g++ src/Map.cpp tests/testMap.cpp -o bin/test
 
 run-test: build-test
 	@ ./bin/test
 
 run: build
-	@ ./bin/$N.out
+	@ ./bin/$N.out "All'sWellThatEndsWell.txt" "OutputFile.txt"
 
 debug:
 	@ g++ *.cpp -o $N.out -ggdb
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 #include <boost/log/trivial.hpp>
 
 #include "Reduce.h"
+#include "Map.h"
 
 // Input Dir
 // Output Dir
@@ -20,6 +24,25 @@ int main(int argc, char *argv[])
     std::string output_dir = "some/path/somewhere";
     Reduce reduce = Reduce(output_dir);
     reduce.reduce("Hello", values);
+
+    // start of JC's main.cpp
+    std::ifstream inputFile(argv[1], std::ios::in);
+
+    size_t bufSize = 1024;
+    Map myMap(argv[2], bufSize);
+
+    if (inputFile.is_open()){
+        std::string line;
+        while(inputFile.good()){
+            getline(inputFile, line);
+            myMap.map(line);
+        }
+        inputFile.close();
+    }
+
+    else{
+        std::cout << "Unable to open file.";
+    }
 
     return 0;
 }
