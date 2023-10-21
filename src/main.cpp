013e35e9d6c9e7b6f65daacfbd21cfef44792ccd
Chandler Johns <chandlerjohns@yahoo.com>
2023-10-21 19:58:30 +0000

    editing main

--- src/main.cpp
+++ src/main.cpp
@@ -42,14 +42,6 @@ int main(int argc, char *argv[])
         }
     }
 
-
-    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-    BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-    BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-    BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-    BOOST_LOG_TRIVIAL(error) << "An error severity message";
-    BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
-
     // std::vector<int> values = {1, 2, 3};
     // std::string output_dir = "some/path/somewhere";
 
