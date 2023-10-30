6f000594f3a53ad7352f08b4aef999d8bd7a9831
squ3ve <squeeve@users.noreply.github.com>
2023-10-30 15:19:47 -0400

    Added some initial tests of output files.

--- tests/testWorkflow.cpp
+++ tests/testWorkflow.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE TestWorkflow
 #include <boost/test/included/unit_test.hpp>
+#include <iostream>
 #include <string>
 
 #include "../src/Workflow.hpp"
@@ -11,6 +12,16 @@ BOOST_AUTO_TEST_CASE(test_workflow__initializes) {
 }
 
 BOOST_AUTO_TEST_CASE(test_workflow__executes) {
+    string test_file = "/test.txt";
     Workflow workflow = Workflow(fileManager);
     workflow.execute();
+    ifstream f(fileManager->getOutputDirectory() + test_file + "-SUCCESS");
+    BOOST_TEST(f.good());
+    f.close();
+
+    ifstream of(fileManager->getOutputDirectory() + test_file + "");
+    string first_line;
+    string expected = "(a, 2)";
+    getline(of, first_line);
+    BOOST_TEST(expected.compare(first_line) == 0);
 }
