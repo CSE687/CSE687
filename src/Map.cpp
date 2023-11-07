c5cd53c66e97eccd702b48e14d6b2ce8e099aa91
Chandler Johns <chandlerjohns@yahoo.com>
2023-11-07 04:07:27 +0000

    simple dlopen and use function

--- src/Map.cpp
+++ src/Map.cpp
@@ -6,76 +6,82 @@
 
 #include "Map.hpp"
 
-#include <boost/algorithm/string.hpp>
-#include <boost/tokenizer.hpp>
+#include <iostream>
 
-// Initialize FileManager instance and reserve buffer space
-Map::Map() {
-    this->fileManager = FileManager::GetInstance();
-    buffer.reserve(bufferSize);
+extern "C" void test(std::string input) {
+    std::cout << "Testing dynamic: " << input << std::endl;
 }
 
-// Tokenize the line from the input file, write each tokenized word to disk, and return number of words tokenized
-int Map::map(std::string filename, std::string line, int numLines, int lineNum) {
-    inputFilename = filename;
-    std::vector<std::string> words = tokenize(line);
-    for (std::string word : words) {
-        exportData(word, numLines, lineNum);
-    }
-    return words.size();
-}
+// #include <boost/algorithm/string.hpp>
+// #include <boost/tokenizer.hpp>
 
-// Create a vector of strings, with each element being a new word
-std::vector<std::string> Map::tokenize(std::string line) {
-    // Treat doubles dashes as spaces
-    boost::algorithm::replace_all(line, "--", " ");
-    std::vector<std::string> words;
-    std::string thisWord;
+// // Initialize FileManager instance and reserve buffer space
+// Map::Map() {
+//     this->fileManager = FileManager::GetInstance();
+//     buffer.reserve(bufferSize);
+// }
 
-    // Remove any invalid ASCII values
-    for (int strInd = 0; strInd < line.length(); strInd++) {
-        if ((line[strInd] < 0) || (line[strInd] > 127)) {
-            line[strInd] = ' ';
-        }
-    }
+// // Tokenize the line from the input file, write each tokenized word to disk, and return number of words tokenized
+// extern "C" int Map::map(std::string filename, std::string line, int numLines, int lineNum) {
+//     inputFilename = filename;
+//     std::vector<std::string> words = tokenize(line);
+//     for (std::string word : words) {
+//         exportData(word, numLines, lineNum);
+//     }
+//     return words.size();
+// }
 
-    // For each character in the line
-    for (char c : line) {
-        // If it's a valid ASCII letter, lowercase it and add it to a temp word
-        if (isalpha(c)) {
-            thisWord += tolower(c);
-        }
-        // If punctuation, ignore
-        else if (ispunct(c)) {
-            continue;
-        }
-        // Append temp word containing only lowercase letters to vector of words
-        else {
-            words.push_back(thisWord);
-            thisWord.clear();
-        }
-    }
+// // Create a vector of strings, with each element being a new word
+// std::vector<std::string> Map::tokenize(std::string line) {
+//     // Treat doubles dashes as spaces
+//     boost::algorithm::replace_all(line, "--", " ");
+//     std::vector<std::string> words;
+//     std::string thisWord;
 
-    // Append word to vector a final time if not empty
-    if (thisWord != "") {
-        words.push_back(thisWord);
-    }
+//     // Remove any invalid ASCII values
+//     for (int strInd = 0; strInd < line.length(); strInd++) {
+//         if ((line[strInd] < 0) || (line[strInd] > 127)) {
+//             line[strInd] = ' ';
+//         }
+//     }
 
-    // Remove any empty lines of data from the vector
-    words.erase(std::remove_if(words.begin(), words.end(), [](const std::string& word) {
-                    return word.empty();
-                }),
-                words.end());
+//     // For each character in the line
+//     for (char c : line) {
+//         // If it's a valid ASCII letter, lowercase it and add it to a temp word
+//         if (isalpha(c)) {
+//             thisWord += tolower(c);
+//         }
+//         // If punctuation, ignore
+//         else if (ispunct(c)) {
+//             continue;
+//         }
+//         // Append temp word containing only lowercase letters to vector of words
+//         else {
+//             words.push_back(thisWord);
+//             thisWord.clear();
+//         }
+//     }
 
-    return words;
-}
+//     // Append word to vector a final time if not empty
+//     if (thisWord != "") {
+//         words.push_back(thisWord);
+//     }
 
-// Buffer output in memory and write to disk when buffer is full or last line of input file is reached
-void Map::exportData(std::string word, int numLines, int lineNum) {
-    buffer += "(" + word + ", 1)\n";
-    if ((buffer.size() >= bufferSize) || (lineNum == numLines - 1)) {
-        std::string tmpFile = this->fileManager->getFileStem(inputFilename) + ".txt";
-        this->fileManager->appendToFile(this->fileManager->getTempDirectory(), tmpFile, buffer);
-        buffer.clear();
-    }
-}
+//     // Remove any empty lines of data from the vector
+//     words.erase(std::remove_if(words.begin(), words.end(), [](const std::string& word) {
+//                     return word.empty();
+//                 }),
+//                 words.end());
+
+//     return words;
+// }
+
+// // Buffer output in memory and write to disk when buffer is full or last line of input file is reached
+// extern "C" void Map::exportData(std::string word, int numLines, int lineNum) {
+//     buffer += "(" + word + ", 1)\n";
+//     if ((buffer.size() >= bufferSize) || (lineNum == numLines - 1)) {
+//         std::string tmpFile = this->fileManager->getFileStem(inputFilename) + ".txt";
+//         this->fileManager->appendToFile(this->fileManager->getTempDirectory(), tmpFile, buffer);
+//         buffer.clear();
+//     }
+// }
--- src/Map.hpp
+++ src/Map.hpp
@@ -1,6 +1,10 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <string>
+
+extern "C" void test(std::string input);
+
 //////////////////////////////////////////////////////////////
 //  Map.hpp     -   header file for Map class               //
 //  Language    -   C++                                     //
@@ -13,38 +17,38 @@ tokenize() method to remove all punctuation, capitalization, and whitespace, and
  method to buffer output in memory and write to disk when the buffer is full.
 */
 
-#include "FileManager.hpp"
+// #include "FileManager.hpp"
 
-class Map{
+// class Map{
 
-    public:
+//     public:
 
-    // class constructor
-    Map();
+//     // class constructor
+//     Map();
 
-    // tokenize string and write to disk
-    int map(std::string, std::string, int, int);
+//     // tokenize string and write to disk
+//     int map(std::string, std::string, int, int);
 
-    // returns a vector of strings, with each element being a new word
-    static std::vector<std::string> tokenize(std::string);
+//     // returns a vector of strings, with each element being a new word
+//     static std::vector<std::string> tokenize(std::string);
 
-    // buffers memory and writes to disk when buffer is full
-    void exportData(std::string, int, int);
+//     // buffers memory and writes to disk when buffer is full
+//     void exportData(std::string, int, int);
 
-    private:
-    // pointer to instance of fileManager class
-    FileManager* fileManager;
+//     private:
+//     // pointer to instance of fileManager class
+//     FileManager* fileManager;
 
-    // stores name of input file
-    std::string inputFilename;
+//     // stores name of input file
+//     std::string inputFilename;
 
-    // constant buffer size for each instance of Map
-    const int bufferSize = 1024;
+//     // constant buffer size for each instance of Map
+//     const int bufferSize = 1024;
 
-    // Buffer to store data before being written to disk
-    std::string buffer;
+//     // Buffer to store data before being written to disk
+//     std::string buffer;
 
-    // Current line number of file that is being mapped
-    int lineNum;
-};
+//     // Current line number of file that is being mapped
+//     int lineNum;
+// };
 #endif
